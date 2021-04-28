05cd1b4b2aee8b885421bc0914f81a776a5d32cb
Alberto Oraá <alberto.oraa@theinformationlab.es>
2021-04-28 17:13:32 +0200

    Estructura de la generacion de energia

---
+++ REData (generacion-estructura)/Public/app.js
@@ -0,0 +1,80 @@
+console.log("¡Funciona!");
+
+(function () {
+  var myConnector = tableau.makeConnector();
+
+  myConnector.getSchema = function (schemaCallback) {
+    const cols = [
+      {
+        id: "ID",
+        dataType: tableau.dataTypeEnum.string,
+      },
+      {
+        id: "Tipologia",
+        dataType: tableau.dataTypeEnum.string,
+      },
+      {
+        id: "FechaHora",
+        dataType: tableau.dataTypeEnum.string,
+      },
+      {
+        id: "Valor",
+        dataType: tableau.dataTypeEnum.int,
+      },
+      {
+        id: "Porcentaje",
+        dataType: tableau.dataTypeEnum.float,
+      },
+      {
+        id: "Ultima_actualizacion",
+        dataType: tableau.dataTypeEnum.string,
+      },
+    ];
+
+    let apiTableSchema = {
+      id: "REData II",
+      alias: "Datos de la REData API para estructura-generacion",
+      columns: cols,
+    };
+
+    schemaCallback([apiTableSchema]);
+  };
+
+  myConnector.getData = function (table, doneCallback) {
+    let tableData = [];
+    var i = 0;
+    var j = 0;
+
+    $.getJSON(
+      "https://apidatos.ree.es/es/datos/generacion/estructura-generacion?start_date=2021-04-27T00:00&end_date=2021-04-27T23:59&time_trunc=day",
+      function (resp) {
+        var apiData = resp.included;
+        // Iterate over the JSON object
+        for (i = 0, len = apiData.length; i < len; i++) {
+          for (j = 0; j < apiData[i].attributes.values.length; j++) {
+            var nestedData = apiData[i].attributes.values[j];
+            tableData.push({
+              FechaHora: nestedData.datetime,
+              Porcentaje: nestedData.percentage,
+              Valor: Number(nestedData.value),
+              Ultima_actualizacion: resp.data.attributes["last-update"],
+              Tipologia: apiData[i].type,
+              ID: apiData[i].id,
+            });
+          }
+        }
+        table.appendRows(tableData);
+        doneCallback();
+      }
+    );
+  };
+
+  tableau.registerConnector(myConnector);
+})();
+
+document.querySelector("#getData").addEventListener("click", getData);
+
+function getData() {
+  tableau.connectionName = "REData";
+  tableau.submit();
+}
